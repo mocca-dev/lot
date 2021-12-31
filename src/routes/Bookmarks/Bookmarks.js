e314154f514dbb6e1a585008dd7ab2b95aa17c10
Damelanote2501 <nicolastonelli1406@gmail.com>
2021-12-31 12:16:49 -0300

    Bookmarks lotlist addition

--- src/routes/Bookmarks/Bookmarks.js
+++ src/routes/Bookmarks/Bookmarks.js
@@ -1,15 +1,89 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import Store from '../../store';
+
+import LotList from '../../components/LotList/LotList';
+import SearchBar from '../../components/SearchBar/SearchBar';
 
 const Bookmarks = () => {
   const { dispatch } = useContext(Store);
+
+  const [results] = useState([
+    {
+      img: '',
+      title: 'Title Test',
+      address: 'Brown 247',
+      date: '',
+      price: '86',
+      since: '12',
+    },
+    {
+      img: '',
+      title: 'Title Test2',
+      address: 'Brown 247',
+      date: '',
+      price: '86',
+      since: '12',
+    },
+    {
+      img: '',
+      title: 'Title Test3',
+      address: 'Brown 247',
+      date: '',
+      price: '86',
+      since: '12',
+    },
+    {
+      img: '',
+      title: 'Title Test3',
+      address: 'Brown 247',
+      date: '',
+      price: '86',
+      since: '12',
+    },
+    {
+      img: '',
+      title: 'Title Test3',
+      address: 'Brown 247',
+      date: '',
+      price: '86',
+      since: '12',
+    },
+    {
+      img: '',
+      title: 'Title Test3',
+      address: 'Brown 247',
+      date: '',
+      price: '86',
+      since: '12',
+    },
+    {
+      img: '',
+      title: 'Title Test3',
+      address: 'Brown 247',
+      date: '',
+      price: '86',
+      since: '12',
+    },
+  ]);
 
   useEffect(() => {
     dispatch({ type: 'SET_SUB_HEADER', payload: 'Guardados' });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return <main>Bookmarks</main>;
+  return (
+    <>
+      <span className="non-scroll-main">
+        <SearchBar />
+        <span className="result-counter">
+          Mostrando {results?.length} de 10
+        </span>
+      </span>
+      <main>
+        <LotList list={results} />
+      </main>
+    </>
+  );
 };
 
 export default Bookmarks;
