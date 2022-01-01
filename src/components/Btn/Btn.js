9c92969f228d566410240fcbcfb9a01157edfa20
Damelanote2501 <nicolastonelli1406@gmail.com>
2022-01-01 17:26:57 -0300

    Profile screen content & first btn creation

---
+++ src/components/Btn/Btn.js
@@ -0,0 +1,7 @@
+import './Btn.css';
+
+const Btn = ({ label }) => {
+  return <button className="btn-container">{label}</button>;
+};
+
+export default Btn;
--- src/routes/Profile/Profile.js
+++ src/routes/Profile/Profile.js
@@ -1,5 +1,8 @@
 import { useContext, useEffect } from 'react';
+import Btn from '../../components/Btn/Btn';
 import Store from '../../store';
+
+import './Profile.css';
 
 const Profile = () => {
   const { dispatch } = useContext(Store);
@@ -9,7 +12,24 @@ const Profile = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return <main>Profile</main>;
+  return (
+    <>
+      <main className="profile-screen-container">
+        <div className="content-container">
+          <img
+            className="profile-pic"
+            src="/icons/profile-mock.png"
+            alt="profile"
+          />
+          <h2>Juan Perez</h2>
+          <span>juanperez@gmail.com</span>
+        </div>
+      </main>
+      <footer className="footer-profile-profile">
+        <Btn label="Cerrar sesión" />
+      </footer>
+    </>
+  );
 };
 
 export default Profile;
