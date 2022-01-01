76d6e03d92ae3beb34b1c07d2302a5ca29ca9332
Damelanote2501 <nicolastonelli1406@gmail.com>
2022-01-01 17:36:05 -0300

    MyLots content addition

---
+++ src/routes/MyLots/MyLots.js
@@ -0,0 +1,79 @@
+import { useContext, useEffect, useState } from 'react';
+
+import LotList from '../../components/LotList/LotList';
+import Store from '../../store';
+
+const MyLots = () => {
+  const { dispatch } = useContext(Store);
+  const [results] = useState([
+    {
+      img: '',
+      title: 'Title Test',
+      address: 'Brown 247',
+      date: '',
+      price: '86',
+      since: '12',
+    },
+    {
+      img: '',
+      title: 'Title Test2',
+      address: 'Brown 247',
+      date: '',
+      price: '86',
+      since: '12',
+    },
+    {
+      img: '',
+      title: 'Title Test3',
+      address: 'Brown 247',
+      date: '',
+      price: '86',
+      since: '12',
+    },
+    {
+      img: '',
+      title: 'Title Test3',
+      address: 'Brown 247',
+      date: '',
+      price: '86',
+      since: '12',
+    },
+    {
+      img: '',
+      title: 'Title Test3',
+      address: 'Brown 247',
+      date: '',
+      price: '86',
+      since: '12',
+    },
+    {
+      img: '',
+      title: 'Title Test3',
+      address: 'Brown 247',
+      date: '',
+      price: '86',
+      since: '12',
+    },
+    {
+      img: '',
+      title: 'Title Test3',
+      address: 'Brown 247',
+      date: '',
+      price: '86',
+      since: '12',
+    },
+  ]);
+
+  useEffect(() => {
+    dispatch({ type: 'SET_SUB_HEADER', payload: 'Mis publicaciones' });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <main>
+      <LotList list={results} />
+    </main>
+  );
+};
+
+export default MyLots;
