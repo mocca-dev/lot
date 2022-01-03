542e624f6a36970583f96310768814ba6d87a5a8
Damelanote2501 <nicolastonelli1406@gmail.com>
2022-01-02 21:29:25 -0300

    Outside componetization

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,10 +1,10 @@
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router';
 
+import OutsideClick from './../OutsideClick/OutsideClick';
 import './Menu.css';
 
 const Menu = ({ showMenu, setShowMenu }) => {
-  const wrapperRef = useRef(null);
   const [menuList] = useState([
     { label: 'Mi perfil', action: '/profile' },
     { label: 'Mis publicaciones', action: '/mylots' },
@@ -17,35 +17,20 @@ const Menu = ({ showMenu, setShowMenu }) => {
     navigate(action);
   };
 
-  useEffect(() => {
-    function handleClickOutside(event) {
-      if (showMenu && wrapperRef && wrapperRef.current.contains(event.target)) {
-        setShowMenu(false);
-      }
-    }
-    document.addEventListener('mousedown', handleClickOutside);
-    return function cleanup() {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  });
-
   return (
-    <>
-      {showMenu && (
-        <span>
-          <span ref={wrapperRef} className="menu-backdrop" />
-          <div className="menu">
-            <ul>
-              {menuList.map((menuItem) => (
-                <li onClick={() => menuClickItem(menuItem.action)}>
-                  {menuItem.label}
-                </li>
-              ))}
-            </ul>
-          </div>
-        </span>
-      )}{' '}
-    </>
+    showMenu && (
+      <OutsideClick action={() => setShowMenu(false)}>
+        <div className="menu">
+          <ul>
+            {menuList.map((menuItem) => (
+              <li onClick={() => menuClickItem(menuItem.action)}>
+                {menuItem.label}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </OutsideClick>
+    )
   );
 };
 
