ceabc9111c511b0a5f3a7b5eac0b1da69454516d
Damelanote2501 <nicolastonelli1406@gmail.com>
2022-01-17 21:14:54 -0300

    Textarea addition to FieldText & publish btn New

--- src/components/FieldText/FieldText.js
+++ src/components/FieldText/FieldText.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './FieldText.css';
 
-const FieldText = ({ placeholder }) => {
+const FieldText = ({ placeholder, isGhost, label, type = 'text' }) => {
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    if (label) setName(label.split(' ')[0]);
+  }, [label]);
+
   return (
-    <input
-      className="fieldtext-container"
-      type="text"
-      name="name"
-      id="mame"
-      placeholder={placeholder}
-    />
+    <>
+      {label && <label>{label}</label>}
+      {type === 'text' ? (
+        <input
+          className={
+            'fieldtext-container ' +
+            (isGhost ? 'border ' : '') +
+            (label ? 'marginTop' : '')
+          }
+          type={type}
+          name={name}
+          id={name}
+          placeholder={placeholder}
+        />
+      ) : (
+        <textarea
+          className={
+            'fieldtext-container ' +
+            (isGhost ? 'border ' : '') +
+            (label ? 'marginTop' : '')
+          }
+          id={name}
+          name={name}
+          rows="4"
+          cols="50"
+        ></textarea>
+      )}
+      <div className="spacer"></div>
+    </>
   );
 };
 
--- src/routes/New/New.js
+++ src/routes/New/New.js
@@ -1,5 +1,32 @@
 import { useContext, useEffect } from 'react';
+import FieldText from '../../components/FieldText/FieldText';
+import Btn from '../../components/Btn/Btn';
 import Store from '../../store';
+
+const showConfirmModal = (dispatch) => {
+  const modalConfig = {
+    title: '¿Está seguro que desea publicar?',
+    show: true,
+    type: undefined,
+    url: '',
+    btns: {
+      left: {
+        action: () => dispatch({ type: 'HIDE_MODAL' }),
+        text: 'Cancelar',
+      },
+      right: {
+        action: () => {
+          // setUser(null);
+          // localStorage.setItem('user', null);
+          // history.push('/ingreso');
+          dispatch({ type: 'HIDE_MODAL' });
+        },
+        text: 'Aceptar',
+      },
+    },
+  };
+  dispatch({ type: 'SET_MODAL', payload: modalConfig });
+};
 
 const New = () => {
   const { dispatch } = useContext(Store);
@@ -9,7 +36,47 @@ const New = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return <main>New</main>;
+  return (
+    <main>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <FieldText
+          placeholder="ej: Evergreen 742"
+          isGhost={true}
+          label="Ubicación"
+        />
+        <FieldText
+          placeholder="ej: (555) 555-5555"
+          isGhost={true}
+          label="Contacto"
+        />
+        <FieldText
+          placeholder="ej: (555) 555-5555"
+          isGhost={true}
+          label="Disponibilidad"
+        />
+        <FieldText
+          placeholder="ej: (555) 555-5555"
+          isGhost={true}
+          label="Tipo de vehículos"
+        />
+        <FieldText
+          placeholder="ej: (555) 555-5555"
+          isGhost={true}
+          label="Tipo de cobertura"
+        />
+        <FieldText
+          placeholder="ej: (555) 555-5555"
+          isGhost={true}
+          label="Descripción"
+          type="textarea"
+        />
+        <Btn
+          onClick={() => showConfirmModal(dispatch)}
+          label="Publicar cochera"
+        />
+      </form>
+    </main>
+  );
 };
 
 export default New;
