dbc5a0b72b951f1075168a6b4398274b0f4f029b
Damelanote2501 <nicolastonelli1406@gmail.com>
2022-02-02 11:05:22 -0300

    New form error msgs addition

--- src/components/FieldText/FieldText.js
+++ src/components/FieldText/FieldText.js
@@ -3,39 +3,47 @@ import React from 'react';
 
 import './FieldText.css';
 
-const FieldText = ({ placeholder, isGhost, label, type = 'text', name }) => {
-  return (
-    <>
-      {label && <label>{label}</label>}
-      {type === 'text' ? (
-        <Field
-          className={
-            'fieldtext-container ' +
-            (isGhost ? 'border ' : '') +
-            (label ? 'marginTop' : '')
-          }
-          type={type}
-          name={name}
-          id={name}
-          placeholder={placeholder}
-        />
-      ) : (
-        <Field
-          as="textarea"
-          className={
-            'fieldtext-container ' +
-            (isGhost ? 'border ' : '') +
-            (label ? 'marginTop' : '')
-          }
-          id={name}
-          name={name}
-          rows="4"
-          cols="50"
-        ></Field>
-      )}
-      <div className="spacer"></div>
-    </>
-  );
-};
+const FieldText = ({
+  placeholder,
+  isGhost,
+  label,
+  type = 'text',
+  name,
+  error,
+  touched,
+}) => (
+  <>
+    {label && <label className="fieldtext-label">{label}</label>}
+    {type === 'text' ? (
+      <Field
+        className={
+          'fieldtext-container ' +
+          (isGhost ? 'border ' : '') +
+          (touched ? 'error-border ' : '') +
+          (label ? 'marginTop' : '')
+        }
+        type={type}
+        name={name}
+        id={name}
+        placeholder={placeholder}
+      />
+    ) : (
+      <Field
+        as="textarea"
+        className={
+          'fieldtext-container ' +
+          (isGhost ? 'border ' : '') +
+          (touched ? 'error-border ' : '') +
+          (label ? 'marginTop' : '')
+        }
+        id={name}
+        name={name}
+        rows="4"
+        cols="50"
+      ></Field>
+    )}
+    <div className="error-msg">{touched ? error : ''}</div>
+  </>
+);
 
 export default FieldText;
--- src/routes/New/New.js
+++ src/routes/New/New.js
@@ -1,10 +1,11 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import * as Yup from 'yup';
 
 import FieldText from '../../components/FieldText/FieldText';
 import Btn from '../../components/Btn/Btn';
 import RadioGroup from '../../components/RadioGroup/RadioGroup';
-import { Form, FormikProvider, useFormik } from 'formik';
+import { Form, Formik } from 'formik';
 import { setModal } from '../../reducers/modal/modalSlice';
 import { set } from '../../reducers/subHeader/subHeaderSlice';
 import { hideFixedContent } from '../../reducers/showFlags/showFlagsSlice';
@@ -29,6 +30,10 @@ const typeOfCoverageList = [
   { label: 'Cubierta', value: '2' },
 ];
 
+const TOO_SHORT_ERROR_MSG = 'Debe tener al menos 2 letras';
+const TOO_LONG_ERROR_MSG = 'Debe tener 50 letras como máximo';
+const REQUIRED_ERROR_MSG = 'Este campo es requerido';
+
 const showConfirmModal = (dispatch, payload) => {
   const modalConfig = {
     title: '¿Está seguro que desea publicar?',
@@ -51,81 +56,115 @@ const showConfirmModal = (dispatch, payload) => {
 const New = () => {
   const dispatch = useDispatch();
 
+  const validationSchema = Yup.object().shape({
+    title: Yup.string()
+      .min(2, TOO_SHORT_ERROR_MSG)
+      .max(50, TOO_LONG_ERROR_MSG)
+      .required(REQUIRED_ERROR_MSG),
+    price: Yup.string()
+      .min(2, TOO_SHORT_ERROR_MSG)
+      .max(50, TOO_LONG_ERROR_MSG)
+      .required(REQUIRED_ERROR_MSG),
+    address: Yup.string()
+      .min(2, TOO_SHORT_ERROR_MSG)
+      .max(50, TOO_LONG_ERROR_MSG)
+      .required(REQUIRED_ERROR_MSG),
+    contact: Yup.string()
+      .min(2, TOO_SHORT_ERROR_MSG)
+      .max(50, TOO_LONG_ERROR_MSG)
+      .required(REQUIRED_ERROR_MSG),
+    description: Yup.string()
+      .min(2, TOO_SHORT_ERROR_MSG)
+      .max(50, TOO_LONG_ERROR_MSG)
+      .required(REQUIRED_ERROR_MSG),
+  });
+
   useEffect(() => {
     dispatch(set('Nueva publicación'));
     dispatch(hideFixedContent());
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const formik = useFormik({
-    initialValues: {
-      title: '',
-      price: '',
-      address: '',
-      contact: '',
-      availability: '',
-      typeOfVehicle: '',
-      typeOfCoverage: '',
-      description: '',
-    },
-    onSubmit: (values) => {
-      showConfirmModal(dispatch, values);
-    },
-  });
-
   return (
     <main>
-      <FormikProvider value={formik}>
-        <Form>
-          <FieldText
-            placeholder="ej: Cochera en alquiler"
-            isGhost={true}
-            label="Título"
-            name="title"
-          />
-          <FieldText
-            placeholder="ej: Evergreen 742"
-            isGhost={true}
-            label="Valor"
-            name="price"
-          />
-          <FieldText
-            placeholder="ej: Evergreen 742"
-            isGhost={true}
-            label="Ubicación"
-            name="address"
-          />
-          <FieldText
-            placeholder="ej: (555) 555-5555"
-            isGhost={true}
-            label="Contacto"
-            name="contact"
-          />
-          <RadioGroup
-            label="Disponibilidad"
-            list={availabilityList}
-            name="availability"
-          />
-          <RadioGroup
-            label="Tipo de vehículos"
-            list={typeOfVehicleList}
-            name="typeOfVehicle"
-          />
-          <RadioGroup
-            label="Tipo de cobertura"
-            list={typeOfCoverageList}
-            name="typeOfCoverage"
-          />
-          <FieldText
-            placeholder="ej: (555) 555-5555"
-            isGhost={true}
-            label="Descripción"
-            type="textarea"
-            name="description"
-          />
-          <Btn label="Publicar cochera" type="submit" />
-        </Form>
-      </FormikProvider>
+      <Formik
+        initialValues={{
+          title: '',
+          price: '',
+          address: '',
+          contact: '',
+          availability: '',
+          typeOfVehicle: '',
+          typeOfCoverage: '',
+          description: '',
+        }}
+        onSubmit={(values) => {
+          showConfirmModal(dispatch, values);
+        }}
+        validationSchema={validationSchema}
+      >
+        {({ errors, touched, isValid }) => (
+          <Form>
+            <FieldText
+              placeholder="ej: Cochera en alquiler"
+              isGhost={true}
+              label="Título"
+              name="title"
+              error={errors.title}
+              touched={touched.title}
+            />
+            <FieldText
+              placeholder="ej: Evergreen 742"
+              isGhost={true}
+              label="Valor"
+              name="price"
+              error={errors.price}
+              touched={touched.price}
+            />
+            <FieldText
+              placeholder="ej: Evergreen 742"
+              isGhost={true}
+              label="Ubicación"
+              name="address"
+              error={errors.address}
+              touched={touched.address}
+            />
+            <FieldText
+              placeholder="ej: (555) 555-5555"
+              isGhost={true}
+              label="Contacto"
+              name="contact"
+              error={errors.contact}
+              touched={touched.contact}
+            />
+            <RadioGroup
+              label="Disponibilidad"
+              list={availabilityList}
+              name="availability"
+            />
+            <RadioGroup
+              label="Tipo de vehículos"
+              list={typeOfVehicleList}
+              name="typeOfVehicle"
+            />
+            <RadioGroup
+              label="Tipo de cobertura"
+              list={typeOfCoverageList}
+              name="typeOfCoverage"
+            />
+            <FieldText
+              placeholder="ej: (555) 555-5555"
+              isGhost={true}
+              label="Descripción"
+              type="textarea"
+              name="description"
+              error={errors.description}
+              touched={touched.description}
+            />
+            <Btn label="Publicar cochera" type="submit" disabled={!isValid} />
+          </Form>
+        )}
+      </Formik>
     </main>
   );
 };
